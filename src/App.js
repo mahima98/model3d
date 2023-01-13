03b4080b2742a07eb16c26cd37a36750b118651c
mahima98 <mahimaramgolam98@gmail.com>
2023-01-13 10:57:25 +0400

    upt readme file

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-
-import './index.css';
-import React, { useEffect, useState } from 'react'
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls, ContactShadows, } from '@react-three/drei';
-import { Suspense, useRef } from 'react';
-import Cup from './Cup';
+import "./index.css";
+import React, { useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, ContactShadows } from "@react-three/drei";
+import { Suspense, useRef } from "react";
+import Cup from "./Cup";
 
 function App() {
-
   return (
-    <div className='App flex bg-red-200'>
+    <div className="App flex bg-red-50">
       <Canvas camera={{ position: [10, 10, 20], fov: 40 }}>
         <ambientLight intensity={0.1} />
         <directionalLight position={[0, 0, 5]} />
-        <ContactShadows opacity={1} scale={20} blur={5} far={6} resolution={256} />
+        <ContactShadows
+          opacity={1}
+          scale={20}
+          blur={5}
+          far={6}
+          resolution={256}
+        />
         <Suspense fallback={null}>
           <Cup position={[-6, 0, 1]} />
           <Cup position={[-3, 0, 0]} />
